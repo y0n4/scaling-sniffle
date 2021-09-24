5e9455d51f380e488035f0f7bbbcd011f6581948
Yona Nagayama <04l.yona@gmail.com>
2021-09-24 14:17:19 -0700

    feat: create new shadow dom web component for chip with some styling and functions

---
+++ play-chip.js
@@ -0,0 +1,125 @@
+'use strict';
+
+(function() {
+  class PlayChip extends HTMLElement {
+    constructor() {
+      // establish prototype chain
+      super();
+
+      // attaches shadow tree and returns shadow root reference
+      // https://developer.mozilla.org/en-US/docs/Web/API/Element/attachShadow
+      const shadow = this.attachShadow({ mode: 'open' });
+
+      // creating a container for the play-chip PlayChip
+      const editableListContainer = document.createElement('div');
+
+      // get attribute values from getters
+      const label = this.label;
+      const isDelete = this.isDelete;
+
+      // adding a class to our container for the sake of clarity
+      editableListContainer.classList.add('play-chip');
+
+      // creating the inner HTML of the editable list element
+      editableListContainer.innerHTML = `
+        <style>
+          .chip {
+            background-color: var(--my-background, #eee);
+            cursor: default;
+            display: flex;
+            width: max-content;
+            align-items: center;
+            border-radius: 30px;
+            padding: 5px;
+            height: 20px;
+            color: black;
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+            font-size: 12px;
+            margin: 5px;
+          }
+          .hidden {
+            display: none;
+          }
+          .disabled {
+            opacity: 0.3;
+          }
+          .outlined {
+            border: 0.5px solid black;
+            background-color: transparent;
+          }
+          ::slotted(*) {
+            object-fit: cover;
+            border-radius: 50%;
+            width: 20px;
+            height: 20px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: var(--icon-background, gainsboro);
+          }
+          p {
+            margin-left: 5px;
+            margin-right: 5px;
+          }
+          button {
+            background-color: #ACACAC;
+            cursor: pointer;
+            border: none;
+            border-radius: 50%;
+            height: 18px;
+            width: 18px;
+            display: flex;
+            align-items: center;
+            color: white;
+            justify-content: center;
+          }
+          button:hover {
+            background-color: #8D8D8D;
+          } 
+        </style>
+        <div id="main-chip">
+          <slot></slot>
+          <p>${label}</p>
+          ${isDelete}
+        </div>
+      `;
+
+      // binding methods
+      this.removeChipItem = this.removeChipItem.bind(this);
+
+      // appending the container to the shadow DOM
+      shadow.appendChild(editableListContainer);
+    }
+
+    // fires after the element has been attached to the DOM
+    connectedCallback() {
+      // determines chip style customization based on passed properties
+      let chipClass = 'chip';
+      const mainChip = this.shadowRoot.querySelector('#main-chip');
+      if (this.getAttribute('isDisabled')) chipClass += ' disabled';
+      if (this.getAttribute('isOutlined')) chipClass += ' outlined';
+      mainChip.className = chipClass;
+
+      // add event handle if there's a delete button on chip
+      const deleteChipButton = this.shadowRoot.querySelector('.remove-chip');
+      deleteChipButton && deleteChipButton.addEventListener('click', this.removeChipItem);
+    }
+
+    get label() {
+      return this.getAttribute('label') || '';
+    }
+    get isDelete() {
+      if (this.getAttribute('isDelete')) {
+        return `<button class="remove-chip">✕</button>`;
+      }
+      return '';
+    }
+
+    removeChipItem(e) {
+      e.target.parentNode.remove();
+    }
+  }
+
+  // let the browser know about the custom element
+  customElements.define('play-chip', PlayChip);
+})();
